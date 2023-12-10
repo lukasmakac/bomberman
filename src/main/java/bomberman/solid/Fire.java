3e0d2a9d0379cd22e8111083dc8777e2d4189a7f
Lukas Maruniak <lukas.maruniak@d-wise.com>
2023-12-10 23:21:47 +0100

    Fire gif changed

--- src/main/java/bomberman/solid/Fire.java
+++ src/main/java/bomberman/solid/Fire.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 
 public class Fire extends SolidEntity {
 
-  public static final Image IMAGE = getImage("fire_test_3.gif");
+  public static final Image IMAGE = getImage("fire_test_2.gif");
   public static final int TIME_TO_CEASE = 2; // time for removal in seconds
 
   public Fire(Point2D position) {
